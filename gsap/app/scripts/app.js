c3ec319a000ad9c5f507d875c1d82ebf0ac73189
Ouedraogo Arsene - on Linux OS <arsene.ouedraogo@centraliens.net>
2016-05-08 16:21:24 +0200

    yeoman generator applied

---
+++ gsap/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name gsapDemoApp
+ * @description
+ * # gsapDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('gsapDemoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
